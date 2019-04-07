1649e6b792edcdac8eeed53f8908785c5cbece2a
juanjo <juanjo@tooltyp.com>
2019-04-07 22:12:25 +0200

    Carrusel y jumboton creado con react bootstrap.

--- reactmarvel/src/App.js
+++ reactmarvel/src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Carousel from 'react-bootstrap/Carousel';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header>
+					
+				</header>
+				<Carousel fade={true}>
+					<Carousel.Item>
+						<img
+						className="d-block w-100"
+						src="http://www.cinefagos.es/wp-content/uploads/2015/04/Marvel.jpg"
+						alt="First slide"
+						/>
+						<Carousel.Caption>
+						<h2>Bienvenido a la App de Marvel</h2>
+						<h4>Que mejor manera de aprender react que con los superheroes preferidos</h4>
+						</Carousel.Caption>
+					</Carousel.Item>
+					<Carousel.Item>
+						<img
+						className="d-block w-100"
+						src="https://hipertextual.com/files/2019/02/hipertextual-escena-post-creditos-capitana-marvel-conectada-con-avengers-4endgame-2019678495-4096x2304.jpg"
+						alt="Third slide"
+						/>
+
+						<Carousel.Caption>
+						<h2>Bienvenido a la App de Marvel</h2>
+						<h4>Que mejor manera de aprender react que con los superheroes preferidos</h4>
+						</Carousel.Caption>
+					</Carousel.Item>
+					<Carousel.Item>
+						<img
+						className="d-block w-100"
+						src="https://img-cdn.hipertextual.com/files/2019/03/hipertextual-nuevo-spoiler-avengers-endgame-adelanta-batalla-monumental-2019674741.jpg?strip=all&lossy=1&quality=60&ssl=1"
+						alt="Third slide"
+						/>
+
+						<Carousel.Caption>
+						<h2>Bienvenido a la App de Marvel</h2>
+						<h4>Que mejor manera de aprender react que con los superheroes preferidos</h4>
+						</Carousel.Caption>
+					</Carousel.Item>
+					</Carousel>
+					<Jumbotron fluid>
+						<Container>
+							<h1>Aprendiendo React</h1>
+							<p>
+								Pues nada que estoy aprendiendo a desarrollar en ReactJs y se me ha ocurrido hacerlo con la tematica marvel
+							</p>
+						</Container>
+					</Jumbotron>;
+			</div>
+		);
+	}
 }
 
 export default App;
